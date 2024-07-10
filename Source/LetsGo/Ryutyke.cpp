6aa69e74a6f480eeaf09c57e02500a7d993543a4
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2024-07-11 02:12:48 +0900

    Commit Test

---
+++ Source/LetsGo/Ryutyke.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ryutyke.h"
+
+// Sets default values
+ARyutyke::ARyutyke()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ARyutyke::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARyutyke::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
