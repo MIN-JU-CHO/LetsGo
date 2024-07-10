b39ba8b3a380f9449a710058c1f8cb7546b93135
Seunghyeon Ryu <play2ski@gmail.com>
2024-07-11 02:02:41 +0900

    Commit Test

---
+++ Source/LetsGo/MinjuCho.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MinjuCho.h"
+
+// Sets default values
+AMinjuCho::AMinjuCho()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMinjuCho::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("I won GameJam in 2023."))
+	
+}
+
+// Called every frame
+void AMinjuCho::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
